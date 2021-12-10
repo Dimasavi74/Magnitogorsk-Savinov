8c64b4d806f646776436d67841ace6829815e656
Dimasavi74 <Dimasavi74comp@gmail.com>
2021-12-10 17:12:48 +0500

    Second commit

--- program.py
+++ program.py
@@ -1,6 +1,9 @@
+#https://github.com/Dimasavi74/Lesson1.git
 def main():
     print('My first git program')
-    print('My first commit')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
 
 
 if __name__ == '__main__':
